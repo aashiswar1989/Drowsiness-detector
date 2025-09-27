33171ae1f584964a925f211ef5e2234d75f74987
Aashish Swarnkar <eceaashish.swarnkar@gmail.com>
2025-09-28 00:38:18 +0530

    First commit of the project

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+PROJECT_NAME = "DrowsinessDetector"
+AUTHOR = "Aashish Swarnkar"
+
+def get_requirements(file: str) -> list:
+    """
+    Reads the requirements from a file and returns them as a list.
+    """
+    with open(file, 'r') as f:
+        requirements = [line.strip() for line in f if line.strip() != "-e ."]
+
+    return requirements
+
+
+setup(
+    name = PROJECT_NAME,
+    version = "0.1.0",
+    author = AUTHOR,
+    install_requires = get_requirements('requirements.txt'),
+    packages = find_packages(),
+    package_dir={"": "src"},
+)
+
